38ec8eb57e2fd8d0386b833355731d0d619eea39
eddieward <eddie@isle3.com>
2013-01-19 07:47:40 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoBongoRails::Application.config.secret_token = 'a81cae06bc9384f520c31df12817c9b7dff2564482a85ad506a616af4fc5e31ecdc3b8c03066e879250e7175a5c9360e6fe28fec26ed3af9f06e102f9bcb4c0b'
